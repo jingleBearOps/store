ffd91de36ca85caae6517947d7b2830a7a0e70c8
jingleBearOps <tjz2834626872@gmail.com>
2023-08-30 16:49:46 -0700

    nav bar

--- reStore/client/src/app/layout/App.tsx
+++ reStore/client/src/app/layout/App.tsx
@@ -1,7 +1,9 @@
-import {Typography} from "@mui/material";
+import {CssBaseline} from "@mui/material";
 import { useEffect, useState } from 'react';
 import { Product } from '../models/product';
 import Catalog from '../../features/catalog/catalog';
+import Header from "./Header";
+import { Container } from "@mui/system";
 const products = [
   {name:'product1', price: 100.00},
   {name:'product2', price: 200.00},
@@ -26,8 +28,11 @@ function App() {
   }
   return (
     <>
-      <Typography variant="h1">Re-Store</Typography>
-      <Catalog products={products} addProduct={addProduct}/>
+      <CssBaseline/>
+      <Header/>
+      <Container>
+        <Catalog products={products} addProduct={addProduct}/>
+      </Container>
     </>
   );
 }
