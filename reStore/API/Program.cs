8097558c90845f9cdbe8b6859fd59c606a688891
Tian Boyd <tianboyd@Tians-MacBook-Pro.local>
2023-08-29 17:11:31 -0700

    seed data migrate data

--- reStore/API/Program.cs
+++ reStore/API/Program.cs
@@ -25,5 +25,13 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
-
+var scope =  app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+var logger= scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+try {
+    context.Database.Migrate();
+    DbInitializer.Initialize(context);
+}catch( Exception ex){
+    logger.LogError(ex, "a problem occured during migration");
+}
 app.Run();
