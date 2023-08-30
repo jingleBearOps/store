fa0cc1c61ad9cecd0345346d561f4ca42ad09425
Tian Boyd <tianboyd@Tians-MacBook-Pro.local>
2023-08-30 14:55:46 -0700

    react init api connected

--- reStore/API/Program.cs
+++ reStore/API/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<StoreContext>(opt =>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,6 +22,9 @@ if (app.Environment.IsDevelopment())
 }
 
 // app.UseHttpsRedirection();
+app.UseCors(opt => {
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+});
 
 app.UseAuthorization();
 
