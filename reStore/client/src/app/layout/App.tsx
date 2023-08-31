c4b8c728d7796c8d43a2dbd9c4acc29ff8d00697
jingleBearOps <tjz2834626872@gmail.com>
2023-08-31 12:57:19 -0700

    dark mode

--- reStore/client/src/app/layout/App.tsx
+++ reStore/client/src/app/layout/App.tsx
@@ -1,39 +1,36 @@
 import {CssBaseline} from "@mui/material";
-import { useEffect, useState } from 'react';
-import { Product } from '../models/product';
 import Catalog from '../../features/catalog/catalog';
 import Header from "./Header";
 import { Container } from "@mui/system";
+import {ThemeProvider, createTheme} from '@mui/material/styles'
+import { useState } from 'react';
 const products = [
   {name:'product1', price: 100.00},
   {name:'product2', price: 200.00},
   {name:'product3', price: 300.00},
 ]
 function App() {
-  const [products, setProducts] = useState<Product []>([]);
-  useEffect(()=>{
-    fetch('http://localhost:5000/api/Products')
-    .then(response => response.json())
-    .then(data => setProducts(data))
-  }, [])
-  function addProduct(){
-    setProducts(prevState => [...prevState, {
-      id: prevState.length  + 1001,
-      name: 'product' + (prevState.length + 1),
-      price: (prevState.length * 100) + 100,
-      brand: 'some brand',
-      description: 'blabla',
-      pictureUrl: 'http://picsum.photos/200'
-    }])
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark': "light"
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background:{
+        default: paletteType === 'light' ?'#eaeaea': '#121212'
+      }
+    }
+  })
+  function handleThemeChange(){
+    setDarkMode(!darkMode);
   }
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline/>
-      <Header/>
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
       <Container>
-        <Catalog products={products} addProduct={addProduct}/>
+        <Catalog />
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
 
--- reStore/client/src/features/catalog/catalog.tsx
+++ reStore/client/src/features/catalog/catalog.tsx
@@ -1,19 +1,20 @@
 import { Fragment } from "react";
-import { Button } from "@mui/material";
 import ProductList from "./ProductList";
 import { Product } from "../../app/models/product"
+import { useEffect, useState } from 'react';
 
-interface Props {
-    products: Product[];
-    addProduct: () => void;
-}
 
-export default function Catalog({products, addProduct}: Props){
+export default function Catalog(){
+    const [products, setProducts] = useState<Product []>([]);
+    useEffect(()=>{
+      fetch('http://localhost:5000/api/Products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+    }, [])
     return (
         <Fragment>
             <h1>Catalog</h1>
             <ProductList products={products}></ProductList>
-            <Button variant="contained" onClick={addProduct}> Add</Button>
       </Fragment>
     )
 }
