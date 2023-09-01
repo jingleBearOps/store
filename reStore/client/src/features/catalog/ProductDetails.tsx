79f153ab481a05497cd3559655ae2504375dd3b9
jingleBearOps <tjz2834626872@gmail.com>
2023-09-01 12:33:23 -0700

    loading component

--- reStore/client/src/features/catalog/ProductDetails.tsx
+++ reStore/client/src/features/catalog/ProductDetails.tsx
@@ -5,6 +5,7 @@ import { Product } from "../../app/models/product";
 import agent from "../../app/api/agent";
 import { NOTFOUND } from "dns";
 import NotFound from "../../app/errors/NotFound";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 export default function ProductDetails(){
     const {id} = useParams<{id: string}>();
     const [product, setProduct] = useState<Product | null>(null);
@@ -15,7 +16,7 @@ export default function ProductDetails(){
             .catch(error => console.log(error))
             .finally(() => {setLoading(false)});
     },[id])
-    if (loading) return <h3>Loading...</h3>
+    if (loading) return <LoadingComponent message='Loading product...' />
     if (!product) return <NotFound/>
     return (
         // <Typography variant='h2'>
--- reStore/client/src/features/catalog/catalog.tsx
+++ reStore/client/src/features/catalog/catalog.tsx
@@ -3,13 +3,19 @@ import ProductList from "./ProductList";
 import { Product } from "../../app/models/product"
 import { useEffect, useState } from 'react';
 import agent from "../../app/api/agent";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 
 
 export default function Catalog(){
     const [products, setProducts] = useState<Product []>([]);
+    const [loading, setLoading] = useState(true);
     useEffect(()=>{
-      agent.Catalog.list().then(products => setProducts(products))
+      agent.Catalog.list()
+      .then(products => setProducts(products))
+      .catch(error => console.log(error))
+      .finally(()=> setLoading(false))
     }, [])
+    if (loading) return <LoadingComponent message='Loading products...'/>
     return (
         <Fragment>
             <h1>Catalog</h1>
