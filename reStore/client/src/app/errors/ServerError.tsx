5ec666952d342a1c6d962bb690d5a2af37799b2f
jingleBearOps <tjz2834626872@gmail.com>
2023-08-31 19:30:55 -0700

    server error render

---
+++ reStore/client/src/app/errors/ServerError.tsx
@@ -0,0 +1,27 @@
+// import { Container } from "@mui/system";
+import { Container,  Typography, Paper, Divider } from "@mui/material";
+import { useLocation } from "react-router-dom";
+
+export default function ServerError() {
+    const {state} = useLocation();
+    return (
+        <Container component={Paper}> 
+        {state?.error ? (
+            <>
+                <Typography gutterBottom variant="h3" color="secondary">
+                    {state.error.title}
+                </Typography>
+                <Divider />
+                <Typography variant = "body1">
+                    {state.error.detail || "Internal server error"}
+                </Typography>
+            </>
+        ):(
+            <Typography gutterBottom variant="h5">
+                Server error
+            </Typography>
+        )}
+            <Typography variant='h5' gutterBottom> Server error</Typography>           
+        </Container>
+    )
+}
--- reStore/client/src/app/router/Routes.tsx
+++ reStore/client/src/app/router/Routes.tsx
@@ -5,6 +5,7 @@ import Catalog from "../../features/catalog/catalog";
 import ProductDetails from "../../features/catalog/ProductDetails";
 import AboutPage from "../../features/about/AboutPage";
 import ContactPage from "../../features/contact/ContactPage";
+import ServerError from "../errors/ServerError";
 export const router = createBrowserRouter([
     {
         path:'/',
@@ -15,6 +16,8 @@ export const router = createBrowserRouter([
             {path: 'catalog/:id', element: <ProductDetails />},
             {path: 'about', element: <AboutPage />},
             {path: 'contact', element: <ContactPage />},
+            {path: 'server-error', element: <ServerError />},
+
         ]
     }
 ])
