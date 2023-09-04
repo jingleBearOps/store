f7d68192ce5373275cdde6a90ca4b36edc0621eb
jingleBearOps <tjz2834626872@gmail.com>
2023-09-04 15:48:58 -0700

    cart functionality all works

--- reStore/client/src/features/basket/BasketPage.tsx
+++ reStore/client/src/features/basket/BasketPage.tsx
@@ -1,9 +1,28 @@
 import { Box, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
-import { Delete } from "@mui/icons-material";
+import { Add, Delete, Remove } from "@mui/icons-material";
 import { useStoreContext } from "../../app/context/StoreContext";
+import { useState } from "react";
+import agent from "../../app/api/agent";
+import LoadingButton from "@mui/lab/LoadingButton";
 
 export default function BasketPage(){
-    const {basket} = useStoreContext();
+    const {basket, setBasket, removeItem} = useStoreContext();
+    const [loading, setLoading] = useState(false);
+    
+    function handleAddItem(productId: number){
+      setLoading(true);
+      agent.Basket.addItem(productId)
+        .then(basket => setBasket(basket))
+        .catch(error => console.log(error))
+        .finally(()=> setLoading(false))
+    }
+    function handleRemoveItem(productId: number, quantity = 1){
+      setLoading(true);
+      agent.Basket.removeItem(productId, quantity)
+        .then(()=> removeItem(productId, quantity))
+        .catch(error => console.log(error))
+        .finally(()=> setLoading(false))
+    }
     if(!basket) return <Typography variant='h3'>Your basket is empty</Typography>
     console.log(basket);
     return (
@@ -13,7 +32,7 @@ export default function BasketPage(){
             <TableRow>
               <TableCell>Product</TableCell>
               <TableCell align="right">Price</TableCell>
-              <TableCell align="right">Quantity</TableCell>
+              <TableCell align="center">Quantity</TableCell>
               <TableCell align="right">Sub total</TableCell>
               <TableCell align="right"></TableCell>
             </TableRow>
@@ -31,12 +50,20 @@ export default function BasketPage(){
                   </Box>
                 </TableCell>
                 <TableCell align="right">${(item.price / 100).toFixed(2)}</TableCell>
-                <TableCell align="right">{item.quantity}</TableCell>
+                <TableCell align="center">
+                  <LoadingButton loading={loading} onClick={()=>handleRemoveItem(item.productId)} color="error">
+                    <Remove/>
+                  </LoadingButton>
+                  {item.quantity}
+                  <LoadingButton loading={loading} onClick={()=>handleAddItem(item.productId)} color="secondary">
+                    <Add/>
+                  </LoadingButton>
+                </TableCell>
                 <TableCell align="right">{(item.price * item.quantity / 100).toFixed(2)}</TableCell>
                 <TableCell align="right">
-                    <IconButton color="error">
+                    <LoadingButton loading={loading} onClick={()=>handleRemoveItem(item.productId, item.quantity)} color="error">
                         <Delete />
-                    </IconButton>
+                    </LoadingButton>
                 </TableCell>
               </TableRow>
             ))}
