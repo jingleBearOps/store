a8a6aa95d08c30fedb6232c423f1cf9c6bab4e6c
jingleBearOps <tjz2834626872@gmail.com>
2023-09-03 20:27:16 -0700

    cookie and backend connect

--- reStore/client/src/features/catalog/ProductCard.tsx
+++ reStore/client/src/features/catalog/ProductCard.tsx
@@ -1,11 +1,21 @@
 import { Avatar, Button, Card, CardActions, CardContent, CardMedia, Typography, CardHeader } from "@mui/material";
 import { Product } from "../../app/models/product"
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import agent from "../../app/api/agent";
+import { LoadingButton } from '@mui/lab';
 
 interface Props {
     product: Product;
 }
 export default function ProductCard({product}: Props){
+  const [loading, setLoading] = useState(false);
+  function handleAddItem(productId: number){
+    setLoading(true);
+    agent.Basket.addItem(productId)
+      .catch(error => console.log(error))
+      .finally(() => setLoading(false));
+  }
     return (
         <Card >
             <CardHeader
@@ -33,7 +43,7 @@ export default function ProductCard({product}: Props){
           </Typography>
         </CardContent>
         <CardActions>
-          <Button size="small">Add to card</Button>
+          <LoadingButton loading={loading} onClick={()=>handleAddItem(product.id)} size="small">Add to card</LoadingButton>
           <Button component={Link}  to={`/catalog/${product.id}`} size="small">View</Button>
         </CardActions>
       </Card>
