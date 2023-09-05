8a227b339d7e588f8965364cd60d4b7cbdc3fc28
jingleBearOps <tjz2834626872@gmail.com>
2023-09-04 17:32:11 -0700

    checkout temp

--- reStore/client/src/app/router/Routes.tsx
+++ reStore/client/src/app/router/Routes.tsx
@@ -8,6 +8,7 @@ import ContactPage from "../../features/contact/ContactPage";
 import ServerError from "../errors/ServerError";
 import NotFound from "../errors/NotFound";
 import BasketPage from "../../features/basket/BasketPage";
+import CheckoutPage from "../../features/checkout/CheckoutPage";
 export const router = createBrowserRouter([
     {
         path:'/',
@@ -21,6 +22,7 @@ export const router = createBrowserRouter([
             {path: 'server-error', element: <ServerError />},
             {path: 'not-found', element: <NotFound />},
             {path: '/basket', element: <BasketPage />},
+            {path: '/checkout', element: <CheckoutPage />},
             {path: '*', element: <Navigate replace to='/not-found' />},
 
         ]
