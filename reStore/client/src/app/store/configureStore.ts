0eecccfde9f7fbf8de5c1e1558971067fd09fb0c
jingleBearOps <tjz2834626872@gmail.com>
2023-09-05 14:26:13 -0700

    redux toolkit

--- reStore/client/src/app/store/configureStore.ts
+++ reStore/client/src/app/store/configureStore.ts
@@ -1,6 +1,21 @@
-import {createStore} from 'redux';
-import counterReducer from '../../features/contact/counterReducer';
 
-export function configureStore() {
-    return createStore(counterReducer)
-}
+import {configureStore} from "@reduxjs/toolkit";
+import { counterSlice } from "../../features/contact/counterSlice";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+
+// export function configureStore() {
+//     return createStore(counterReducer)
+// }
+
+export const store = configureStore({
+    reducer: {
+        counter: counterSlice.reducer
+    }
+}) // a friendly abstraction over the standard redux createStore()
+
+
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
