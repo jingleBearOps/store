bb82d673a8b1d37615cf0aa5050f35fc7aec3221
jingleBearOps <tjz2834626872@gmail.com>
2023-09-05 15:55:45 -0700

    redux connected

--- reStore/client/src/app/layout/App.tsx
+++ reStore/client/src/app/layout/App.tsx
@@ -9,27 +9,29 @@ import { Outlet } from "react-router";
 import { getCookie } from "../util/util";
 import agent from "../api/agent";
 import LoadingComponent from "./LoadingComponent";
-import { useStoreContext } from "../context/StoreContext";
-const products = [
-  {name:'product1', price: 100.00},
-  {name:'product2', price: 200.00},
-  {name:'product3', price: 300.00},
-]
+// import { useStoreContext } from "../context/StoreContext";
+import { setBasket } from "../../features/basket/basketSlice";
+import { useAppDispatch } from "../store/configureStore";
+// const products = [
+//   {name:'product1', price: 100.00},
+//   {name:'product2', price: 200.00},
+//   {name:'product3', price: 300.00},
+// ]
 function App() {
-  const {setBasket} = useStoreContext();
+  const dispatch  = useAppDispatch();
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const buyerId = getCookie('buyerId');
     if (buyerId) {
       agent.Basket.get()
-        .then(basket => setBasket(basket))
+        .then(basket => dispatch(setBasket(basket)))
         .catch(error => console.log(error))
         .finally(() => setLoading(false));
     }else{
       setLoading(false);
     }
-  },[setBasket])
+  },[dispatch])
   const [darkMode, setDarkMode] = useState(false);
   const paletteType = darkMode ? 'dark': "light"
   const theme = createTheme({
