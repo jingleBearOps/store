d52027c5e782d4e0e9a9bbae48ccff8e024880e2
jingleBearOps <tjz2834626872@gmail.com>
2023-09-06 16:10:31 -0700

    unique brands and type API imple

--- reStore/API/Controllers/ProductsController.cs
+++ reStore/API/Controllers/ProductsController.cs
@@ -43,5 +43,13 @@ namespace API.Controllers
             return product;
             // return await _context.Products.FindAsync(id);
         }
+        [HttpGet("filters")]
+        public async Task<IActionResult> GetFilters()
+        {
+            var brands = await _context.Products.Select(p => p.Brand).Distinct().ToListAsync();
+            var types = await _context.Products.Select(p => p.Type).Distinct().ToListAsync();
+
+            return Ok(new {brands, types});
+        }
     }
 }
