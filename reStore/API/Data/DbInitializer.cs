f1c0158a9b4c9f2f1e23e538c4debecc793ca806
jingleBearOps <tjz2834626872@gmail.com>
2023-09-12 16:21:02 -0700

    entity configuation and migration

--- reStore/API/Data/DbInitializer.cs
+++ reStore/API/Data/DbInitializer.cs
@@ -1,11 +1,33 @@
 
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.OpenApi.Any;
 
 namespace API.Data
 {
     public static class DbInitializer
     {
-        public static void Initialize(StoreContext context){
+        public static async Task Initialize(StoreContext context, UserManager<User> userManager){
+            if(!userManager.Users.Any())
+            {
+                var user = new User
+                {
+                    UserName = "bob",
+                    Email = "bob@test.com"
+                };
+
+                await userManager.CreateAsync(user, "Pa$$w0rd");
+                await userManager.AddToRoleAsync(user, "Member");
+
+                var admin = new User
+                {
+                    UserName = "admin",
+                    Email = "admin@test.com"
+                };
+                await userManager.CreateAsync(admin, "Pa$$w0rd");
+                await userManager.AddToRolesAsync(admin, new[] {"Member", "Admin"});
+            }
+
             if(context.Products.Any())  return;
             var products = new List<Product>
             {
@@ -211,6 +233,11 @@ namespace API.Data
                 context.Products.Add(product);
             }
             context.SaveChanges();
+        }
+
+        internal static Task Initialize(StoreContext context)
+        {
+            throw new NotImplementedException();
         }
     }
 }
