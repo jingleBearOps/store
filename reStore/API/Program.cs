5181368f6af5cb0c23a7024ed7e8242e95dc2ef9
jingleBearOps <tjz2834626872@gmail.com>
2023-09-13 14:05:35 -0700

    jwt decode success

--- reStore/API/Program.cs
+++ reStore/API/Program.cs
@@ -1,6 +1,7 @@
 using API.Data;
 using API.Entities;
 using API.Middleware;
+using API.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
@@ -24,7 +25,7 @@ builder.Services.AddIdentityCore<User>(opt =>
 
 builder.Services.AddAuthentication();
 builder.Services.AddAuthorization();
-
+builder.Services.AddScoped<TokenService>();
 
     
 var app = builder.Build();
