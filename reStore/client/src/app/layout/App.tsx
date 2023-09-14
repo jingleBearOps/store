37ecc332304ebc1cb474f37bafdeb4c75ad8ba29
jingleBearOps <tjz2834626872@gmail.com>
2023-09-14 11:42:53 -0700

    code clean up

--- reStore/client/src/app/layout/App.tsx
+++ reStore/client/src/app/layout/App.tsx
@@ -2,15 +2,13 @@ import {CssBaseline} from "@mui/material";
 import Header from "./Header";
 import { Container } from "@mui/system";
 import {ThemeProvider, createTheme} from '@mui/material/styles'
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Outlet } from "react-router";
-import { getCookie } from "../util/util";
-import agent from "../api/agent";
 import LoadingComponent from "./LoadingComponent";
 // import { useStoreContext } from "../context/StoreContext";
-import { setBasket } from "../../features/basket/basketSlice";
+import { fetchBasketAsync, setBasket } from "../../features/basket/basketSlice";
 import { useAppDispatch } from "../store/configureStore";
 import { fetchCurrentUser } from "../../features/account/accountSlice";
 // const products = [
@@ -23,18 +21,18 @@ function App() {
   const dispatch  = useAppDispatch();
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const buyerId = getCookie('buyerId');
-    dispatch(fetchCurrentUser());
-    if (buyerId) {
-      agent.Basket.get()
-        .then(basket => dispatch(setBasket(basket)))
-        .catch(error => console.log(error))
-        .finally(() => setLoading(false));
-    }else{
-      setLoading(false);
+  const initApp = useCallback(async () => {
+    try{
+      await dispatch(fetchCurrentUser());
+      await dispatch(fetchBasketAsync());
+    }catch(error){
+      console.log(error)
     }
-  },[dispatch])
+  }, [dispatch])
+
+  useEffect(() => {
+    initApp().then(()=> setLoading(false))
+  },[initApp])
   const [darkMode, setDarkMode] = useState(false);
   const paletteType = darkMode ? 'dark': "light"
   const theme = createTheme({
