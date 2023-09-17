ed0d31349970608b0972c9db8be2e29a392e1ab6
jingleBearOps <tjz2834626872@gmail.com>
2023-09-16 17:25:11 -0700

    temp

--- reStore/client/src/features/checkout/AddressForm.tsx
+++ reStore/client/src/features/checkout/AddressForm.tsx
@@ -1,6 +1,7 @@
 import { Typography, Grid, TextField, FormControlLabel, Checkbox, Button } from "@mui/material";
 import { useForm, useFormContext } from "react-hook-form";
 import AppTextInput from "../../app/components/AppTextInput";
+import AppCheckbox from "../../app/components/AppCheckbox";
 
 export default function AddressForm() {
     const {control} = useFormContext();
@@ -32,10 +33,7 @@ export default function AddressForm() {
             <AppTextInput control={control} name="country"  label="Country" />
         </Grid>
         <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
-            label="Use this address for payment details"
-          />
+            <AppCheckbox name='saveAddress' label="save this as the default address" control={control}/>
         </Grid>
       </Grid>
       <Button type='submit'> Submit Form</Button>
