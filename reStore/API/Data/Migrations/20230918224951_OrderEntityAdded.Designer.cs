0d9f5b284a95b15d3a88f126b99330d994147f17
jingleBearOps <tjz2834626872@gmail.com>
2023-09-18 15:53:35 -0700

    workinf version

--- reStore/API/Data/Migrations/20230918224951_OrderEntityAdded.Designer.cs
+++ reStore/API/Data/Migrations/20230918224951_OrderEntityAdded.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    [Migration("20230915224626_OrderEntityAdded")]
+    [Migration("20230918224951_OrderEntityAdded")]
     partial class OrderEntityAdded
     {
         /// <inheritdoc />
