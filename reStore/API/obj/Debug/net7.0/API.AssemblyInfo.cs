d1a1402a42dc40461eaddc011765b6cc5bde6ebf
jingleBearOps <tjz2834626872@gmail.com>
2023-09-20 13:53:42 -0700

    store stripe key paris in safe storage

--- reStore/API/obj/Debug/net7.0/API.AssemblyInfo.cs
+++ reStore/API/obj/Debug/net7.0/API.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("e16af26c-af82-4a0e-b8ed-aedbf5377e89")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
