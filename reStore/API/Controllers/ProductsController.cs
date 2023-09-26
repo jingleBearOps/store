14ddd65953340817d3a455d2d009d372c6a2de2c
jingleBearOps <tjz2834626872@gmail.com>
2023-09-26 13:13:10 -0700

    cloudinary end point complete

--- reStore/API/Controllers/ProductsController.cs
+++ reStore/API/Controllers/ProductsController.cs
@@ -86,7 +86,7 @@ namespace API.Controllers
 
         [Authorize(Roles = "Admin")]
         [HttpPut]
-        public async Task<ActionResult> UpdareProduct(UpdateProductDto productDto)
+        public async Task<ActionResult<Product>> UpdareProduct([FromForm]UpdateProductDto productDto)
         {
             var product = await _context.Products.FindAsync(productDto.Id);
             
@@ -94,9 +94,23 @@ namespace API.Controllers
 
             _mapper.Map(productDto, product);
 
+            if(productDto.File != null)
+            {
+                var imageResult = await _imageService.AddImageAsync(productDto.File);
+
+                if(imageResult.Error != null) return BadRequest(new ProblemDetails{Title = imageResult.Error.Message});
+
+                if(!string.IsNullOrEmpty(product.PublicId)) 
+                    await _imageService.DeleteImageAsync(product.PublicId);
+
+                product.PictureUrl = imageResult.SecureUrl.ToString();
+
+                product.PublicId = imageResult.PublicId;
+            }
+
             var result = await _context.SaveChangesAsync() > 0;
 
-            if (result) return NoContent();
+            if (result) return Ok(product);
 
             return BadRequest(new ProblemDetails{Title = "Problem updating products"});
         }
@@ -109,6 +123,9 @@ namespace API.Controllers
             var product = await _context.Products.FindAsync(id);
 
             if (product == null) return NotFound();
+
+            if(!string.IsNullOrEmpty(product.PublicId)) 
+                await _imageService.DeleteImageAsync(product.PublicId);
 
             _context.Products.Remove(product);// data base is not changed cause it is not saved
 
