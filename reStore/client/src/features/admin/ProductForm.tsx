633222b6cc171c1c3b3096b928a9a0ef0f257050
jingleBearOps <tjz2834626872@gmail.com>
2023-09-26 15:58:35 -0700

    product add form

--- reStore/client/src/features/admin/ProductForm.tsx
+++ reStore/client/src/features/admin/ProductForm.tsx
@@ -1,51 +1,71 @@
 import { Typography, Grid, Paper, Box, Button } from "@mui/material";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 import AppTextInput from "../../app/components/AppTextInput";
 import { Product } from "../../app/models/product";
 import { useEffect } from "react";
+import useProducts from "../../app/hooks/useProducts";
+import AppSelectList from "../../app/components/AppSelectList";
+import AppDropzone from "../../app/components/AppDropzone";
 
 interface Props {
     product?: Product;
     cancelEdit: () => void;
 }
 export default function ProductForm({product, cancelEdit}: Props) {
-    const { control, reset } = useForm();
+    const { control, reset, handleSubmit } = useForm();
+    const {brands, types} = useProducts(); 
 
     useEffect(() => {
         if(product) reset(product);
     },[product, reset])
+
+    function handleSubmitData(data: FieldValues){
+        console.log(data);
+    }
     return (
         <Box component={Paper} sx={{p: 4}}>
             <Typography variant="h4" gutterBottom sx={{mb: 4}}>
                 Product Details
             </Typography>
+            <form onSubmit={handleSubmit(handleSubmitData)}>
             <Grid container spacing={3}>
                 <Grid item xs={12} sm={12}>
                     <AppTextInput control={control} name='name' label='Product name' />
                 </Grid>
                 <Grid item xs={12} sm={6}>
+                    <AppSelectList control={control} items = {brands} name='brand' label='Brand' />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <AppSelectList control={control} items = {types} name='type' label='Type' />
+                </Grid>
+{/* 
+                above: dropdown select
+                bottom: customized input */}
+
+                {/* <Grid item xs={12} sm={6}>
                     <AppTextInput control={control} name='brand' label='Brand' />
                 </Grid>
                 <Grid item xs={12} sm={6}>
                     <AppTextInput control={control} name='type' label='Type' />
+                </Grid> */}
+                <Grid item xs={12} sm={6}>
+                    <AppTextInput type='number' control={control} name='price' label='Price' />
                 </Grid>
                 <Grid item xs={12} sm={6}>
-                    <AppTextInput control={control} name='price' label='Price' />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <AppTextInput control={control} name='quantityInStock' label='Quantity in Stock' />
+                    <AppTextInput type = 'number' control={control} name='quantityInStock' label='Quantity in Stock' />
                 </Grid>
                 <Grid item xs={12}>
-                    <AppTextInput control={control} name='description' label='Description' />
+                    <AppTextInput multiline={true} rows={4} control={control} name='description' label='Description' />
                 </Grid>
                 <Grid item xs={12}>
-                    <AppTextInput control={control} name='pictureUrl' label='Image' />
+                    <AppDropzone control={control} name='file'  />
                 </Grid>
             </Grid>
             <Box display='flex' justifyContent='space-between' sx={{mt: 3}}>
                 <Button onClick={cancelEdit}variant='contained' color='inherit'>Cancel</Button>
-                <Button variant='contained' color='success'>Submit</Button>
+                <Button type = 'submit' variant='contained' color='success'>Submit</Button>
             </Box>
+            </form>
         </Box>
     )
 }
